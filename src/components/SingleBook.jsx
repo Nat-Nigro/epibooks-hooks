cef8fa2fab5986dd31da85559bd91a79f35d48d4
Nat-Nigro <natalenigro15@gmail.com>
2024-01-09 16:20:22 +0100

    first commit

---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,32 @@
+import { Component, useState } from "react";
+import { Card } from "react-bootstrap";
+import { useState } from "react";
+// import CommentArea from './CommentArea'
+
+const SingleBook = (props) => {
+  // state = {
+  //   selected: false,
+  // }
+
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <>
+      <Card
+        // onClick={() => this.setState({ selected: !this.state.selected })}
+        onClick={() => props.changeSelectedBook(props.book.asin)}
+        style={{
+          border: props.selected === props.book.asin ? "3px solid red" : "none",
+        }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+      {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
+    </>
+  );
+};
+
+export default SingleBook;
